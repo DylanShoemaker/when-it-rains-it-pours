5957b7a8e890052097b2ededea120975270e75b9
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-31 20:41:27 -0600

    append search results to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,9 @@ var tempOutput = document.getElementById("output-temp")
 var windOutput = document.getElementById("output-wind")
 var humidityOutput = document.getElementById("output-humidity")
 var uvOutput = document.getElementById("output-uv")
-var iconEl = document.getElementById("iconEl")
 var weatherIcon = document.getElementById("icon")
+var buttonList = document.getElementById("button-list")
+
 formEl.addEventListener("submit",function(event){
   event.preventDefault();
   //use the name search api to search for the cities coordinates
@@ -17,6 +18,12 @@ formEl.addEventListener("submit",function(event){
   .then(response => response.json())
   .then(data => {
     cityOutput.innerText = data.name;
+
+    var listEl = document.createElement('li');
+    listEl.appendChild(document.createTextNode(data.name));
+    buttonList.appendChild(listEl);
+
+
     var latEl = data.coord.lat.toFixed(2);
     var lonEl = data.coord.lon.toFixed(2);
     //use the coordinate search api to pull up the data because the name search api does not include UV Index info
@@ -29,7 +36,7 @@ formEl.addEventListener("submit",function(event){
       uvOutput.innerText = data.current.uvi;
       iconCode = data.current.weather[0].icon;
       var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-      $("#icon").html("<img src='" + iconUrl  + "'>");
+      $("#icon").html("<img src='" + iconUrl  + "'>");  //https://www.reddit.com/r/FreeCodeCamp/comments/4con5s/how_do_i_use_the_icon_given_in_the_open_weather/
 
 
     })
