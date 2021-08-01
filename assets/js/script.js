365b9eb45e0fe38734b4b2060c156d78f3527bb9
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-31 20:03:47 -0600

    include 2 more api calls for UV index and weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,40 @@ var cityName = document.getElementById("city-name")
 var button = document.getElementById('btn');
 var formEl = document.getElementById("user-form")
 var cityOutput = document.getElementById("output-city")
+var tempOutput = document.getElementById("output-temp")
 var windOutput = document.getElementById("output-wind")
 var humidityOutput = document.getElementById("output-humidity")
 var uvOutput = document.getElementById("output-uv")
-
-
-
-
-
+var iconEl = document.getElementById("iconEl")
+var weatherIcon = document.getElementById("icon")
 formEl.addEventListener("submit",function(event){
   event.preventDefault();
+  //use the name search api to search for the cities coordinates
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName.value+'&units=imperial&appid=3cdd7f0cff5a4ed1af7ae0635d8af1e2' )
   .then(response => response.json())
   .then(data => {
-    
-    console.log(data.name);
-    console.log(cityOutput);
     cityOutput.innerText = data.name;
-    console.log(data);
-  })
+    var latEl = data.coord.lat.toFixed(2);
+    var lonEl = data.coord.lon.toFixed(2);
+    //use the coordinate search api to pull up the data because the name search api does not include UV Index info
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ latEl +"&lon="+ lonEl +"&exclude=hourly,daily&units=imperial&appid=3cdd7f0cff5a4ed1af7ae0635d8af1e2" )
+    .then(response => response.json())
+    .then(data => {
+      tempOutput.innerText = data.current.temp;
+      windOutput.innerText = data.current.wind_speed;
+      humidityOutput.innerText = data.current.humidity;
+      uvOutput.innerText = data.current.uvi;
+      iconCode = data.current.weather[0].icon;
+      var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      $("#icon").html("<img src='" + iconUrl  + "'>");
 
+
+    })
+    
+
+
+  })
+  
 
 
 
@@ -31,6 +45,9 @@ formEl.addEventListener("submit",function(event){
 });
 
 
+
+
+$("#currentDay").text(moment().format("(M/D/YYYY)"));
 
 
 
